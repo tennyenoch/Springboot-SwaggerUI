4d055220357b070d57372928e8a501ee56ba1928
Tenny <tennyenoch@gmail.com>
2020-12-01 20:53:14 -0600

    Initial commit

---
+++ src/main/java/com/swagger/demo/Contact.java
@@ -0,0 +1,25 @@
+package com.swagger.demo;
+
+public class Contact {
+private String id;
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPhone() {
+	return phone;
+}
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+private String name;
+private String phone;
+}
